e6afa0daf4f26cb9328fac6f7a63df7156078823
Your mato <you@example.com>
2021-01-16 00:55:17 +0800

    Initial Commit

---
+++ Collectmytracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Collectmytracker
+//
+//  Created by kittawat phuangsombat on 2021/1/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
