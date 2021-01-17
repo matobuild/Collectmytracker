3be0c252fcb0cf2a30abd03026b856704391391f
Your mato <you@example.com>
2021-01-17 18:46:14 +0800

    -finish creating the grid for data

--- Collectmytracker/Controller/MainTrackerController.swift
+++ Collectmytracker/Controller/MainTrackerController.swift
@@ -10,11 +10,61 @@ import UIKit
 import CoreData
 
 class MainTrackerController: UITableViewController {
-    //    var categories = [Category]()
-    //
-    //    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+
     var tracker = [Tracker]()
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadTrackers()
+    }
+
+    //MARK: - TableView Datasource Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return tracker.count
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TrackerCell", for: indexPath)
+        
+        cell.textLabel?.text = tracker[indexPath.row].name
+        
+        return cell
+        
+    }
+//    //MARK: - TableView Delegate Methods
+//
+//    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        performSegue(withIdentifier: "goToItems", sender: self)
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let destinationVC = segue.destination as! TodoListViewController
+//
+//        if let indexPath = tableView.indexPathForSelectedRow {
+//            destinationVC.selectedCategory = categories[indexPath.row]
+//        }
+//    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToTrackerData", sender: self)
+    }
+    
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        let destinationVC = segue.destination as! TrackerDataViewController
+//
+//        if let indexPath = tableView.indexPathForSelectedRow{
+//            destinationVC
+//        }
+//        		
+//    }
     
     //MARK: - Data Manipulation Methods
     
@@ -25,6 +75,19 @@ class MainTrackerController: UITableViewController {
             print("Error saving category \(error)")
         }
         
+        tableView.reloadData()
+        
+    }
+    func loadTrackers() {
+        
+        let request : NSFetchRequest<Tracker> = Tracker.fetchRequest()
+        
+        do{
+            tracker = try context.fetch(request)
+        } catch {
+            print("Error loading trackers \(error)")
+        }
+       
         tableView.reloadData()
         
     }
---
+++ Collectmytracker/Controller/MyCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  MyCollectionViewCell.swift
+//  Collectmytracker
+//
+//  Created by kittawat phuangsombat on 2021/1/17.
+//
+
+import UIKit
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+}
