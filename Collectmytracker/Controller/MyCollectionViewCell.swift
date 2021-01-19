e03407003645decc9a555b807e96a215b319a681
Your mato <you@example.com>
2021-01-19 08:46:02 +0800

    -made the grid clickable

--- Collectmytracker/Controller/MyCollectionViewCell.swift
+++ Collectmytracker/Controller/MyCollectionViewCell.swift
@@ -8,5 +8,5 @@
 import UIKit
 class MyCollectionViewCell: UICollectionViewCell {
     
-    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
 }
--- Collectmytracker/Controller/TrackerDataViewController.swift
+++ Collectmytracker/Controller/TrackerDataViewController.swift
@@ -5,44 +5,111 @@
 //  Created by kittawat phuangsombat on 2021/1/16.
 //
 
+
 import UIKit
+import CoreData
 
 class TrackerDataViewController: UIViewController {
     
-    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
-    var items = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48"]
+    var itemArray = [Item]()
     
+    var selectedTracker : Tracker?{
+        didSet{
+            loadItems()
+        }
+    }
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    
+    
+    let reuseIdentifier = "cell" // also enter this string as the cell identifier in the storyboard
+   
+    var items = [0]
+    
+    
+    //MARK: - Model Manipulation Methods
+    func saveItems() {
+        
+        do {
+          try context.save()
+        } catch {
+           print("Error saving context \(error)")
+        }
+        //    NEED TO RELOAD GRID
+//        self.tableView.reloadData()
+    }
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil){
+        
+        let trackerPredicate = NSPredicate(format: "parentTracker.name MATCHES %@", selectedTracker!.name!)
+        
+        if let additionalPredeicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [trackerPredicate,additionalPredeicate])
+        }else{
+            request.predicate = trackerPredicate
+        }
+        
+        do{
+            itemArray = try context.fetch(request)
+        }catch{
+            print("Error fetching data from context \(error)")
+        }
+        //    NEED TO RELOAD GRID
+        //    tableView.reloadData()
+        
+    }
 }
+
+
 //MARK: - creating the grid
 
 extension TrackerDataViewController: UICollectionViewDataSource,UICollectionViewDelegate{
     
-        // MARK: - UICollectionViewDataSource protocol
-        
-        // tell the collection view how many cells to make
-        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return self.items.count
-        }
-        
-        // make a cell for each cell index path
-        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            
-            // get a reference to our storyboard cell
-            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
-            
-            // Use the outlet in our custom class to get a reference to the UILabel in the cell
-            cell.myLabel.text = self.items[indexPath.row] // The row value is the same as the index of the desired text within the array.
-            cell.backgroundColor = UIColor.cyan // make cell more visible in our example project
-            
-            return cell
-        }
-        
-        // MARK: - UICollectionViewDelegate protocol
-        
-        func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-            // handle tap events
-            print("You selected cell #\(indexPath.item)!")
-        }
+    // MARK: - UICollectionViewDataSource protocol
+    
+    // tell the collection view how many cells to make
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
     }
     
+    // make a cell for each cell index path
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a reference to our storyboard cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath as IndexPath) as! MyCollectionViewCell
+        
+        // Use the outlet in our custom class to get a reference to the UILabel in the cell
+//        cell.myLabel.text = String(self.items[indexPath.row])// The row value is the same as the index of the desired text within the array.
+        if indexPath.item == items.count-1{
+            cell.myImage.image = #imageLiteral(resourceName: "offline")
+        }else{
+            cell.myImage.image = #imageLiteral(resourceName: "online")
+        }
+        print("idexpath. item is \(indexPath.item)")
+        print("items.count is \(items.count)")
+        return cell
+    }
+    
+    // MARK: - UICollectionViewDelegate protocol
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // handle tap events
+        
+        print("You selected cell #\(indexPath.item)!")
+        
+        if indexPath.item == (items.count-1){
+            items.append(items.last!+1)
+//            print(items)
+            collectionView.reloadData()
+        }else{
+            //ask for confirmation before remvpve(need to do)
+            items.remove(at: indexPath.item)
+//            print(items)
+            collectionView.reloadData()
+        }
+        
+        
+        
+    }
+    
+}
 
